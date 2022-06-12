184db43e786cbb5b19dfc678cc6f6648aa84aa7d
yojhf <106582217+yojhf@users.noreply.github.com>
2022-06-12 23:02:51 +0900

    1_Stage_Logic_01

---
+++ Assets/BrokenVector/LowPolyDungeon/Demo Scenes/Scrpits/DoorTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+      
+            door.transform.localEulerAngles = new Vector3(0, 0, 0);
+        
+    }
+}
